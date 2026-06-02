0d6b42e6cd81afa490982f8e6ad80ff4c54a638a
G-leviathan-O <andrewgoluba@yandex.ru>
2026-06-02 18:13:03 +0300

    Homework3

---
+++ Homework3/Models/City.cs
@@ -0,0 +1,55 @@
+﻿namespace Homework3.Models
+{
+    /// <summary>
+    /// Город
+    /// </summary>
+    public class City
+    {
+        /// <summary>
+        /// ID города
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// ID страны
+        /// </summary>
+        public int CountryId { get; set; }
+
+        /// <summary>
+        /// Название города
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        private int _population;
+
+        /// <summary>
+        /// Население в тысячах
+        /// </summary>
+        public int PopulationK
+        {
+            get => _population;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Population cannot be negative");
+
+                _population = value;
+            }
+        }
+
+        /// <summary>
+        /// Страна
+        /// </summary>
+        public Country? Country { get; set; }
+
+        public City(int id, int countryId, string name, int populationK)
+        {
+            Id = id;
+            CountryId = countryId;
+            Name = name;
+            PopulationK = populationK;
+        }
+
+        public City() : this(0, 0, "", 0) { }
+    }
+}
---
+++ Homework3/Models/Country.cs
@@ -0,0 +1,31 @@
+﻿namespace Homework3.Models
+{
+    /// <summary>
+    /// Страна
+    /// </summary>
+    public class Country
+    {
+        /// <summary>
+        /// ID страны
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Название страны
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Города страны
+        /// </summary>
+        public ICollection<City> Cities { get; set; } = new List<City>();
+
+        public Country(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public Country() : this(0, "") { }
+    }
+}
